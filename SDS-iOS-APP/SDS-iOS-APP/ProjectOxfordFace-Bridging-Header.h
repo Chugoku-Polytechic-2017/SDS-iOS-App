88dfcc2c7b75f6e847639c4bd87a3d98760d6090
石川諒 <ishikawa.pro@gmail.com>
2018-01-14 22:23:25 +0900

    add ProjectOxfordFace-Bridging-Header.h

---
+++ SDS-iOS-APP/SDS-iOS-APP/ProjectOxfordFace-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ProjectOxfordFace-Bridging-Header.h
+//  SDS-iOS-APP
+//
+//  Created by 石川諒 on 2018/01/14.
+//  Copyright © 2018年 石川諒. All rights reserved.
+//
+
+#ifndef ProjectOxfordFace_Bridging_Header_h
+#define ProjectOxfordFace_Bridging_Header_h
+
+#import <ProjectOxfordFace/MPOFaceSDK.h>
+
+#endif /* ProjectOxfordFace_Bridging_Header_h */
