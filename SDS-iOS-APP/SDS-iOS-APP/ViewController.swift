6126830de560724bd6f5888251cb4b35880786d3
石川諒 <ishikawa.pro@gmail.com>
2018-01-14 21:01:38 +0900

    create SDS-iOS-App project

---
+++ SDS-iOS-APP/SDS-iOS-APP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDS-iOS-APP
+//
+//  Created by 石川諒 on 2018/01/14.
+//  Copyright © 2018年 石川諒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
