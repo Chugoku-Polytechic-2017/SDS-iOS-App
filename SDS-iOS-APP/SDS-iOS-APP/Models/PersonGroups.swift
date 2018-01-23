5ae7e376b1e3c82ef1e5e62eb2a7f8262d80c9a1
石川諒 <ishikawa.pro@gmail.com>
2018-01-24 01:25:40 +0900

    add PersonGroups.swift

---
+++ SDS-iOS-APP/SDS-iOS-APP/Models/PersonGroups.swift
@@ -0,0 +1,22 @@
+//
+//  PersonGroups.swift
+//  SDS-iOS-APP
+//
+//  Created by 石川諒 on 2018/01/24.
+//  Copyright © 2018年 石川諒. All rights reserved.
+//
+
+import Foundation
+import ProjectOxfordFace
+import Keys
+
+struct PersonGroups: FaceAPIType {
+    var keys: SDSIOSAPPKeys
+    var faceAPIClient: MPOFaceServiceClient
+    init() {
+        keys = SDSIOSAPPKeys()
+        faceAPIClient = MPOFaceServiceClient(endpointAndSubscriptionKey: keys.fACEAPIURL, key: keys.fACEAPIKEY)
+    }
+
+    
+}
