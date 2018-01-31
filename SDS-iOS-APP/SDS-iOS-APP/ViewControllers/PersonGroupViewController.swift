7070a112968623752bb5adcc9c4bb8e1fefaf86e
石川諒 <ishikawa.pro@gmail.com>
2018-01-31 21:40:12 +0900

    personの追加アクションをSDSAlertViewを使った実装へ修正

--- SDS-iOS-APP/SDS-iOS-APP/ViewControllers/PersonGroupViewController.swift
+++ SDS-iOS-APP/SDS-iOS-APP/ViewControllers/PersonGroupViewController.swift
@@ -46,36 +46,19 @@ class PersonGroupViewController: UITableViewController {
     }
     
     @IBAction func addButtonTapped(_ sender: Any) {
-        let alert = UIAlertController(title: "新しいユーザーを追加", message: "ユーザー名とuserDataを入力してください", preferredStyle: UIAlertControllerStyle.alert)
-        alert.addTextField { textField in
-            textField.tag = 0
-            textField.placeholder = "ユーザー名"
+        let deleteAlert = alertView.addAlert (
+            title: "新しいユーザーを追加",
+            message: "ユーザー名とuserDataを入力してください") {
+                (action, name, userData) in
+                self.faceAPIClient.createPerson(
+                    toPersonGroup: self.personGroup.personGroupId,
+                    name: name,
+                    userData: userData,
+                    response: {
+                    self.fetchPersonList()
+                })
         }
-        alert.addTextField { textField in
-            textField.tag = 1
-            textField.placeholder = "userData(optional)"
-        }
-
-        let action = UIAlertAction(title: "OK", style: .default) { _ in
-            guard let paramsTextField = alert.textFields else {
-                return
-            }
-            let params = paramsTextField.map({ textField -> String? in
-                textField.text
-            })
-            guard let name = params[0] else {
-                return
-            }
-            let userData = params[1]
-            self.faceAPIClient.createPerson(toPersonGroup: self.personGroup.personGroupId, name: name, userData: userData, response: {
-                self.fetchPersonList()
-            })
-
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        alert.addAction(action)
-        alert.addAction(cancelAction)
-        present(alert, animated: true, completion: nil)
+        present(deleteAlert, animated: true, completion: nil)
     }
     
     @IBAction func deleteButtonTapped(_ sender: Any) {
