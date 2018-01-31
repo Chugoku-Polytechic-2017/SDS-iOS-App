e942f4e61dce2ec06ce2f3b15c48cb47a4d50904
石川諒 <ishikawa.pro@gmail.com>
2018-01-31 21:39:34 +0900

    追加用アラートをSDSAlertView.swiftへ追加

--- SDS-iOS-APP/SDS-iOS-APP/View/SDSAlertView.swift
+++ SDS-iOS-APP/SDS-iOS-APP/View/SDSAlertView.swift
@@ -34,5 +34,37 @@ struct SDSAlertView {
         return alertController
     }
 
+    func addAlert(title: String, message: String, handler: @escaping (UIAlertAction,String,String?) -> ()) -> UIAlertController {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: UIAlertControllerStyle.alert
+        )
+
+        alertController.addTextField { textField in
+            textField.placeholder = "ユーザー名"
+        }
+        alertController.addTextField { textField in
+            textField.placeholder = "userData(optional)"
+        }
+
+        let action = UIAlertAction(title: "OK", style: .default) { action in
+            guard let paramsTextField = alertController.textFields else {
+                return
+            }
+            let params = paramsTextField.map({ textField -> String? in
+                textField.text
+            })
+            guard let name = params[0] else {
+                return
+            }
+            let userData = params[1]
+            handler(action, name, userData)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(action)
+        alertController.addAction(cancelAction)
+        return alertController
+    }
     
 }
